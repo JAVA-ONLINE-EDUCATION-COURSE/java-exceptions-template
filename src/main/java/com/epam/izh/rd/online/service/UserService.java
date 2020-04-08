b959663938637818b351c6b93ad072c35dcfcecb
ntnew <ntnew@yandex.ru>
2020-04-08 16:10:30 +0400

    done

--- src/main/java/com/epam/izh/rd/online/service/UserService.java
+++ src/main/java/com/epam/izh/rd/online/service/UserService.java
@@ -1,5 +1,8 @@
 package com.epam.izh.rd.online.service;
 
+import com.epam.izh.rd.online.exception.NotAccessException;
+import com.epam.izh.rd.online.exception.SimplePasswordException;
+import com.epam.izh.rd.online.exception.UserAlreadyRegisteredException;
 import com.epam.izh.rd.online.entity.User;
 import com.epam.izh.rd.online.repository.IUserRepository;
 import com.epam.izh.rd.online.repository.UserRepository;
@@ -30,13 +33,17 @@ public class UserService implements IUserService {
      * @param user - даныне регистрирующегося пользователя
      */
     @Override
-    public User register(User user) {
+    public User register(User user) throws UserAlreadyRegisteredException, SimplePasswordException {
 
-        //
-        // Здесь необходимо реализовать перечисленные выше проверки
-        //
-
-        // Если все проверки успешно пройдены, сохраняем пользователя в базу
+        if (user.getPassword().isEmpty() || user.getLogin().isEmpty()) {
+            throw new IllegalArgumentException("Ошибка в заполнении полей");
+        }
+        if (userRepository.findByLogin(user.getLogin()) != null) {
+            throw new UserAlreadyRegisteredException("Пользователь с логином '" + user.getLogin() + "' уже зарегистрирован");
+        }
+        if (user.getPassword().matches("\\d+")) {
+            throw new SimplePasswordException("Пароль не соответствует требованиям безопасности");
+        }
         return userRepository.save(user);
     }
 
@@ -58,11 +65,12 @@ public class UserService implements IUserService {
      *
      * @param login
      */
-    public void delete(String login) {
-
-        // Здесь необходимо сделать доработку метод
-
+    public void delete(String login) throws NotAccessException {
+        try {
             userRepository.deleteByLogin(login);
+        }catch (UnsupportedOperationException e){
+            throw new NotAccessException("Недостаточно прав для выполнения операции");
+        }
 
         // Здесь необходимо сделать доработку метода
 
