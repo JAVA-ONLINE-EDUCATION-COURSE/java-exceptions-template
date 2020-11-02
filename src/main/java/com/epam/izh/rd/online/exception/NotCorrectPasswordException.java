4b8f6ef7160353fb46915a77430fbae0ca334250
Mikhail Zotov <themisterread@gmail.com>
2020-11-02 23:47:36 +0400

    Completed the exceptions exercise

---
+++ src/main/java/com/epam/izh/rd/online/exception/NotCorrectPasswordException.java
@@ -0,0 +1,8 @@
+package com.epam.izh.rd.online.exception;
+
+public class NotCorrectPasswordException extends RuntimeException {
+
+  public NotCorrectPasswordException(String message) {
+    super(message);
+  }
+}
--- src/main/java/com/epam/izh/rd/online/service/AuthenticationService.java
+++ src/main/java/com/epam/izh/rd/online/service/AuthenticationService.java
@@ -1,6 +1,8 @@
 package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.entity.User;
+import com.epam.izh.rd.online.exception.NotCorrectPasswordException;
+import com.epam.izh.rd.online.exception.UserNotFoundException;
 import com.epam.izh.rd.online.repository.IUserRepository;
 
 public class AuthenticationService implements IAuthenticationService {
@@ -26,16 +28,16 @@ public class AuthenticationService implements IAuthenticationService {
      */
     @Override
     public User login(User user) {
-        // Находим пользователя в базе
         User foundUser = userRepository.findByLogin(user.getLogin());
+        if (foundUser == null) {
+            throw new UserNotFoundException("Пользователь с таким логином не найден");
+        }
 
-        //
-        // Здесь необходимо реализовать перечисленные выше проверки
-        //
+        if (!user.getPassword().equals(foundUser.getPassword())) {
+            throw new NotCorrectPasswordException("Пароль введен неверно!");
+        }
 
-        // Устанавливаем найденного пользователя, который прошел все проверки, как вошедшего в систему.
         CurrentUserManager.setCurrentLoggedInUser(foundUser);
-
         return foundUser;
     }
 
