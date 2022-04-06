31744e3f8c525784039853db0326c81762ad0cac
serg1971ch12@gmail.com <https://gitlab.skillbox.ru>
2022-04-06 17:50:16 +0500

    Это задание по исключениям в Java

--- src/main/java/com/epam/izh/rd/online/JavaExceptions.java
+++ src/main/java/com/epam/izh/rd/online/JavaExceptions.java
@@ -1,6 +1,12 @@
 package com.epam.izh.rd.online;
 
 
+import com.epam.izh.rd.online.entity.User;
+import com.epam.izh.rd.online.exception.NotAccessException;
+import com.epam.izh.rd.online.exception.SimplePasswordException;
+import com.epam.izh.rd.online.exception.UserAlreadyRegisteredException;
+import com.epam.izh.rd.online.repository.IUserRepository;
+
 public class JavaExceptions {
 
     public static void main(String[] args) {
